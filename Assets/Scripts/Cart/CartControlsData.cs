1e47adaf7263de090cc630a706747dd4a9082a50
Lucas Baran <lucas.baran7@gmail.com>
2025-10-04 01:30:47 +0200

    add CartControlsData

---
+++ Assets/Scripts/Cart/CartControlsData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace LD58.Cart
+{
+    [CreateAssetMenu(fileName = "SO_CartControlsData", menuName = "LD58/Cart/Controls Data")]
+    public class CartControlsData : ScriptableObject
+    {
+        [SerializeField] private float _movementSpeed = 1f;
+        [SerializeField] private float _levelBoundsPadding = 2f;
+        [SerializeField] private float _aimSpeed = 50f;
+        [SerializeField] private float _maxAimAngle = 50f;
+
+        public float MovementSpeed => _movementSpeed;
+        public float LevelBoundsPadding => _levelBoundsPadding;
+        public float AimSpeed => _aimSpeed;
+        public float MaxAimAngle => _maxAimAngle;
+    }
+}
