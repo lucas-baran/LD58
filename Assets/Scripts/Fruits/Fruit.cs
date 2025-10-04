73f861adefe3536cc70df32dbbd4f264e6b425bb
Lucas Baran <lucas.baran7@gmail.com>
2025-10-04 13:36:08 +0200

    fruits: handle multiple grow steps
    - sprite changes
    - collisions can be disabled

--- Assets/Scripts/Fruits/Fruit.cs
+++ Assets/Scripts/Fruits/Fruit.cs
@@ -6,18 +6,48 @@ namespace LD58.Fruits
     {
         [SerializeField] private FruitData _data = null;
         [SerializeField] private SpriteRenderer _renderer = null;
+        [SerializeField] private Collider2D _collider = null;
         [SerializeField] private Rigidbody2D _rigidbody = null;
+
+        private int _currentGrowIndex = 0;
 
         public FruitData Data => _data;
 
-        private void OnHit()
+        public CollectedFruit GetCollectedFruit()
         {
-            _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+            return new CollectedFruit(_data, _currentGrowIndex);
+        }
+
+        public void Grow()
+        {
+            if (!IsFullyGrown())
+            {
+                _currentGrowIndex++;
+                RefreshGrowState();
+            }
         }
 
         public void Destroy()
         {
-            Destroy(gameObject);
+            _currentGrowIndex = 0;
+            RefreshGrowState();
+        }
+
+        public bool IsFullyGrown()
+        {
+            return _currentGrowIndex >= _data.MaxGrow;
+        }
+
+        private void RefreshGrowState()
+        {
+            FruitData.GrowStep grow_step = _data.GrowSteps[_currentGrowIndex];
+            _renderer.sprite = grow_step.Sprite;
+            _collider.enabled = grow_step.HasCollisions;
+        }
+
+        private void OnHit()
+        {
+            _rigidbody.bodyType = RigidbodyType2D.Dynamic;
         }
 
         private void OnCollisionEnter2D(
@@ -29,7 +59,8 @@ namespace LD58.Fruits
 
         private void Awake()
         {
-            _renderer.sprite = _data.Sprite;
+            _currentGrowIndex = 0;
+            RefreshGrowState();
         }
     }
 }
--- Assets/Scripts/Fruits/FruitData.cs
+++ Assets/Scripts/Fruits/FruitData.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace LD58.Fruits
@@ -6,11 +7,21 @@ namespace LD58.Fruits
     public class FruitData : ScriptableObject
     {
         [SerializeField] private string _name = string.Empty;
-        [SerializeField] private int _value = 1;
-        [SerializeField] private Sprite _sprite;
+        [SerializeField] private List<GrowStep> _growSteps;
 
         public string Name => _name;
-        public int Value => _value;
-        public Sprite Sprite => _sprite;
+        public int MaxGrow => _growSteps.Count - 1;
+        public IReadOnlyList<GrowStep> GrowSteps => _growSteps;
+
+        public sealed class GrowStep
+        {
+            [SerializeField] private int _value = 1;
+            [SerializeField] private Sprite _sprite;
+            [SerializeField] private bool _hasCollisions;
+
+            public int Value => _value;
+            public Sprite Sprite => _sprite;
+            public bool HasCollisions => _hasCollisions;
+        }
     }
 }
