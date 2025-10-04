efbc65e1e43e56870b48aacb9e422c1ec2443d3f
Lucas Baran <lucas.baran7@gmail.com>
2025-10-04 22:02:32 +0200

    add SceneReference

---
+++ Assets/Scripts/Utilities/SceneReference.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace LD58
+{
+    [Serializable]
+    public sealed class SceneReference
+#if UNITY_EDITOR
+        : ISerializationCallbackReceiver
+#endif
+    {
+        [SerializeField, HideInInspector] private string _sceneName;
+
+        public string SceneName => _sceneName;
+
+#if UNITY_EDITOR
+        [SerializeField] private UnityEditor.SceneAsset _sceneAsset;
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+            _sceneName = _sceneAsset.name;
+        }
+#endif
+    }
+}
