9e01cd79c0f4cc08e92fd7d351a5672f32804fee
Lucas Baran <lucas.baran7@gmail.com>
2025-10-04 21:01:33 +0200

    add UIEndPanel

---
+++ Assets/Scripts/UI/UIEndPanel.cs
@@ -0,0 +1,62 @@
+using LD58.Levels;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LD58.UI
+{
+    public sealed class UIEndPanel : MonoBehaviour
+    {
+        [SerializeField] private EndText _winText;
+        [SerializeField] private EndText _loseText;
+        [SerializeField] private TMP_Text _mainText;
+        [SerializeField] private TMP_Text _quoteText;
+        [SerializeField] private Button _replayButton;
+
+        private void Show(EndText end_text)
+        {
+            gameObject.SetActive(true);
+            _mainText.text = end_text.Main;
+            _quoteText.text = end_text.Quote;
+        }
+
+        private void Level_OnLose()
+        {
+            Show(_loseText);
+        }
+
+        private void Level_OnWin()
+        {
+            Show(_winText);
+        }
+
+        private void ReplayButton_OnClick()
+        {
+            Debug.Log("replay!");
+        }
+
+        private void Start()
+        {
+            Level.Instance.OnLose += Level_OnLose;
+            Level.Instance.OnWin += Level_OnWin;
+            _replayButton.onClick.AddListener(ReplayButton_OnClick);
+            gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            _replayButton.onClick.RemoveListener(ReplayButton_OnClick);
+        }
+
+        [Serializable]
+        private sealed class EndText
+        {
+            [SerializeField] private string _main = string.Empty;
+            [SerializeField] private string _quote = string.Empty;
+
+            public string Main => _main;
+            public string Quote => _quote;
+        }
+    }
+}
