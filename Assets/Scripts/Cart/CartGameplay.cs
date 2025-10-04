f3e7a35c152ef83d9ae7a86f20c5dd838ec5676c
Lucas Baran <lucas.baran7@gmail.com>
2025-10-04 17:51:38 +0200

    CartGameplay: lose skeleton when there is no fruit to shoot

--- Assets/Scripts/Cart/CartGameplay.cs
+++ Assets/Scripts/Cart/CartGameplay.cs
@@ -2,6 +2,7 @@ using Cysharp.Threading.Tasks;
 using LD58.Fruits;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace LD58.Cart
 {
@@ -10,6 +11,8 @@ namespace LD58.Cart
         [SerializeField] private CartControls _cartControls;
 
         private FruitGrower _fruitGrower;
+
+        public event UnityAction OnLose = null;
 
         private async UniTaskVoid PrepareNextShotAsync(CancellationToken cancellation_token)
         {
@@ -24,9 +27,19 @@ namespace LD58.Cart
             await UniTask.WaitWhile(_fruitGrower, fruit_grower => fruit_grower.IsAnyFruitMoving(), cancellationToken: cancellation_token);
         }
 
+        private void Lose()
+        {
+            OnLose?.Invoke();
+        }
+
         private void CartCannon_OnShot()
         {
             PrepareNextShotAsync(destroyCancellationToken).Forget();
+        }
+
+        private void Cannon_OnHasNoFruitToShoot()
+        {
+            Lose();
         }
 
         private void Start()
@@ -35,11 +48,13 @@ namespace LD58.Cart
 
             _cartControls.Cannon.CanShoot = true;
             _cartControls.Cannon.OnShot += CartCannon_OnShot;
+            _cartControls.Cannon.OnHasNoFruitToShoot += Cannon_OnHasNoFruitToShoot;
         }
 
         private void OnDestroy()
         {
             _cartControls.Cannon.OnShot -= CartCannon_OnShot;
+            _cartControls.Cannon.OnHasNoFruitToShoot -= Cannon_OnHasNoFruitToShoot;
         }
     }
 }
