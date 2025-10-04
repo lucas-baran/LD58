0a4f8f99009a17f50e7f1b0b01bd4912dbb17a16
Lucas Baran <lucas.baran7@gmail.com>
2025-10-04 12:52:05 +0200

    add CartBasketCollector

---
+++ Assets/Scripts/Cart/CartBasketCollector.cs
@@ -0,0 +1,20 @@
+using LD58.Fruits;
+using LD58.Players;
+using UnityEngine;
+
+namespace LD58.Cart
+{
+    public class CartBasketCollector : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(
+            Collider2D collider
+            )
+        {
+            if (collider.TryGetComponent(out Fruit fruit))
+            {
+                Player.Instance.Inventory.CollectFruit(fruit);
+                fruit.Destroy();
+            }
+        }
+    }
+}
