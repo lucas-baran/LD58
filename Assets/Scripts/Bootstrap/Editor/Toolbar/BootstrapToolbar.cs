f105a26bd310ceb63aa0f08d195f340713ac1332
Lucas Baran <lucas.baran7@gmail.com>
2026-02-21 19:38:29 +0100

    add BootstrapToolbar static class

---
+++ Assets/Scripts/Bootstrap/Editor/Toolbar/BootstrapToolbar.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEditor.Toolbars;
+
+namespace LucasBaran.Bootstrap.Toolbar
+{
+    internal static class BootstrapToolbar
+    {
+        private const string ELEMENTS_PATH = "Bootstrap";
+
+        [MainToolbarElement(ELEMENTS_PATH, defaultDockPosition = MainToolbarDockPosition.Middle)]
+        private static IEnumerable<MainToolbarElement> GetToolbarElements()
+        {
+            yield return PlayButtonFactory.Create();
+        }
+    }
+}
