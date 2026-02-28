4dafd04dd0f232ff9edb3ba8ae241f20c5a8bc43
Lucas Baran <lucas.baran7@gmail.com>
2026-02-28 19:30:16 +0100

    test GameTimeGeneration build

--- Assets/Scripts/LucasBaran/GameTime/Runtime/Generation/Project~/GameTimeGeneration/obj/Release/netstandard2.0/GameTimeGeneration.AssemblyInfo.cs
+++ Assets/Scripts/LucasBaran/GameTime/Runtime/Generation/Project~/GameTimeGeneration/obj/Release/netstandard2.0/GameTimeGeneration.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameTimeGeneration")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b35b4efe8dd5c27f9809f98a381109014fb0b4be")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameTimeGeneration")]
