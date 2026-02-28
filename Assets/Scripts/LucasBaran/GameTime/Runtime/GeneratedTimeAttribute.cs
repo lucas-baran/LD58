0a189e80e3deced31cdbde46183d1f887ad1f8c6
Lucas Baran <lucas.baran7@gmail.com>
2026-02-28 20:11:08 +0100

    GeneratedTimeAttribute: attribute usage on fields

--- Assets/Scripts/LucasBaran/GameTime/Runtime/GeneratedTimeAttribute.cs
+++ Assets/Scripts/LucasBaran/GameTime/Runtime/GeneratedTimeAttribute.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace LucasBaran.GameTime
 {
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
     public sealed class GeneratedTimeAttribute : Attribute
     {
     }
