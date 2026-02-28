74fd6b255eb1d5dd58f5b84ab2447603b38e1c16
Lucas Baran <lucas.baran7@gmail.com>
2026-02-28 18:27:08 +0100

    add GeneratedTimeAttribute

---
+++ Assets/Scripts/LucasBaran/GameTime/Runtime/Generation/GeneratedTimeAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace LucasBaran.GameTime.Generation
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class GeneratedTimeAttribute : Attribute
+    {
+    }
+}
