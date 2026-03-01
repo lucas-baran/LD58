9cb2068ceef76d9b57a95b12ea3b5f8465a66d72
Lucas Baran <lucas.baran7@gmail.com>
2026-03-01 21:57:05 +0100

    add CreateAssetMenuItems constants class

---
+++ Assets/Scripts/Common/Constants/CreateAssetMenuItems.cs
@@ -0,0 +1,12 @@
+namespace LD58.Common
+{
+    public static class CreateAssetMenuItems
+    {
+        public const string ROOT = "LD58/";
+        public const string LEVELS = ROOT + "Levels/";
+        public const string FRUITS = ROOT + "Fruits/";
+        public const string FRUIT_EFFECTS = FRUITS + "Effects/";
+        public const string CART = ROOT + "Cart/";
+        public const string ENEMIES = ROOT + "Enemies/";
+    }
+}
