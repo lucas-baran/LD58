f18254036b721d737514f7fa0074bb9b95e8d75c
Lucas Baran <lucas.baran7@gmail.com>
2026-03-29 23:48:16 +0200

    Scenario: add modules
    modules are any type of asset reference by an addressable reference which can be retrieved using a unique id

--- Assets/_Project/Scripts/LucasBaran/Bootstrap/Runtime/Scenarios/Scenario.cs
+++ Assets/_Project/Scripts/LucasBaran/Bootstrap/Runtime/Scenarios/Scenario.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace LucasBaran.Bootstrap
 {
@@ -11,11 +12,28 @@ namespace LucasBaran.Bootstrap
         [SerializeField] private bool _showInDebugGui = true;
         [SerializeField] private ScenarioGroup[] _dependencies;
         [SerializeField] private SceneReference[] _sceneReferences;
+        [SerializeField] private Module[] _modules;
 
         public string Name => string.IsNullOrEmpty(_name) ? name : _name;
         public bool ShowInDebugGui => _showInDebugGui;
         public IReadOnlyList<ScenarioGroup> Dependencies => _dependencies;
         public IReadOnlyList<SceneReference> SceneReferences => _sceneReferences;
+
+        public bool TryGetModule(int id, out AssetReference asset_reference)
+        {
+            int module_count = _modules.Length;
+            bool found_module = false;
+            asset_reference = null;
+
+            for (int module_index = 0; !found_module && module_index < module_count; module_index++)
+            {
+                Module module = _modules[module_index];
+                found_module = module.Id == id;
+                asset_reference = module.AssetReference;
+            }
+
+            return found_module;
+        }
 
         [Serializable]
         public sealed class SceneReference
@@ -27,6 +45,16 @@ namespace LucasBaran.Bootstrap
             public bool ActiveScene => _activeScene;
             public int Priority => _priority;
             public SceneAssetReference SceneAssetReference => _sceneAssetReference;
+        }
+
+        [Serializable]
+        public sealed class Module
+        {
+            [SerializeField] private int _id;
+            [SerializeField] private AssetReference _assetReference;
+
+            public int Id => _id;
+            public AssetReference AssetReference => _assetReference;
         }
     }
 }
